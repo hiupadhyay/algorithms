4678378f7573a81fde6be2884cdb7b8dac561d53
hiupadhyay <hiupadhyay@paypal.com>
2021-06-10 23:25:46 +0530

    Design LRU Cache

---
+++ src/main/java/important/LRUCache.java
@@ -0,0 +1,16 @@
+package important;
+
+class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+        return -1;
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
