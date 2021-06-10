fbc835bdc226e199329490ca2a2d6217fedbd001
hiupadhyay <hiupadhyay@paypal.com>
2021-06-10 19:53:16 +0530

    Threads- Print evenn odd

---
+++ src/main/java/sync/DataStore.java
@@ -0,0 +1,27 @@
+package sync;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DataStore {
+
+    private final Sync sync = new Sync();
+
+    private Map<String, User> map = new HashMap<>();
+
+    User save(User u) {
+        return sync.write(() -> {
+            return map.put(u.getUid(), u);
+        });
+
+    }
+}
+
+class User {
+    String uid;
+
+    public String getUid() {
+        return uid;
+    }
+}
