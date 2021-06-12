928fd77d90bc202a6600e32eaaedf5b04e1d0a1e
hiupadhyay <hiupadhyay@paypal.com>
2021-06-12 17:40:07 +0530

    Powerset- Bitwise logic

--- src/main/java/medium/PowerSet.java
+++ src/main/java/medium/PowerSet.java
@@ -1,9 +1,31 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PowerSet {
+
+
+    public List<List<Integer>> getPowerSet_2(int[] input) {
+        List<List<Integer>> output = new ArrayList<>();
+        int start = 0;
+        int end = (int) Math.pow(2, input.length);
+        for (int i = start; i < end; i++) {
+            List<Integer> subset = new ArrayList<>();
+            int temp = i;
+            for (int j = input.length - 1; j >= 0; j--) {
+                int r = temp % 2;
+                temp = temp / 2;
+                if (r != 0) {
+                    subset.add(input[j]);
+                }
+            }
+            output.add(subset);
+        }
+        return output;
+    }
+
 
     public List<List<Integer>> getPowerSet(int[] input) {
         List<List<Integer>> output = new ArrayList<>();
@@ -23,7 +45,8 @@ public class PowerSet {
     public static void main(String[] args) {
         int[] input = new int[]{1, 2, 3};
         PowerSet powerSet = new PowerSet();
-        System.out.println(powerSet.getPowerSet(input));
+        //System.out.println(Arrays.toString(powerSet.getBinary(4)));
+        System.out.println(powerSet.getPowerSet_2(input));
 
     }
 }
