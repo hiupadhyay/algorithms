ce8ccde71cb7742e4c8483e21bf70ce39f12c197
hiupadhyay <hiupadhyay@paypal.com>
2021-06-13 16:40:46 +0530

    Find duplicate in array in o(N) and o(1) memory

---
+++ src/main/java/important/FindDuplicateNumberWithModifiyingArrray.java
@@ -0,0 +1,33 @@
+package important;
+
+/**
+ * Note this algo is based on Cycle detection algo
+ * Idea is to get a first overlap due to cycle.
+ * Once we have overlap we run both fast and slow pace where they reach it means cycle starts there.
+ */
+public class FindDuplicateNumberWithModifiyingArrray {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{1, 3, 4, 2, 2,
+        };
+
+        System.out.println(findCycle(input));
+    }
+
+    private static int findCycle(int[] nums) {
+        if (nums.length < 1) return -1;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        //we found the overlap...
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+    }
+}
