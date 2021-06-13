f856cd175d1bf3c76e26cc46e72fe96c6233bfc6
hiupadhyay <hiupadhyay@paypal.com>
2021-06-13 14:38:08 +0530

    Fixed review comments

--- src/main/java/important/LowestCommonSubSequenceLength.java
+++ src/main/java/important/LowestCommonSubSequenceLength.java
@@ -26,24 +26,24 @@ public class LowestCommonSubSequenceLength {
         for (int[] ints : dp) {
             Arrays.fill(ints, -1);
         }
-        return recursionUsingMemo(text1, text2, 0, 0, dp);
+        return recursionUsingMemo(text1, text2, dp, 0, 0);
     }
 
-    public int recursionUsingMemo(String text1, String text2, int i, int j, int[][] dp) {
+    public int recursionUsingMemo(String text1, String text2, int[][] dp, int i, int j) {
         if (i > text1.length() - 1 || j > text2.length() - 1)
             return 0;
         if (dp[i][j] != -1) return dp[i][j];
         if (text1.charAt(i) == text2.charAt(j)) {
-            dp[i][j] = 1 + recursionUsingMemo(text1, text2, i + 1, j + 1, dp);
+            dp[i][j] = 1 + recursionUsingMemo(text1, text2, dp, i + 1, j + 1);
         } else {
-            dp[i][j] = Math.max(recursionUsingMemo(text1, text2, i + 1, j, dp),
-                    recursionUsingMemo(text1, text2, i, j + 1, dp));
+            dp[i][j] = Math.max(recursionUsingMemo(text1, text2, dp, i + 1, j),
+                    recursionUsingMemo(text1, text2, dp, i, j + 1));
         }
         return dp[i][j];
     }
 
     public int longestCommonSubsequenceUsingDP(String text1, String text2) {
-        int dp[][] = new int[text1.length() + 1][text2.length() + 1];
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[i].length; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
